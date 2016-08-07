73a90840d7e21ce120ba69198c776a933f7e1fb8
leverage2014 <myletter428@gmail.com>
2016-08-06 23:34:02 -0400

    Update todos on firebase

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -40,10 +40,11 @@ export var startAddTodo = (text)=>{
 	};
 };
 
-export var toggleTodo = (id)=>{
+export var updateTodo = (id, updates)=>{
 	return {
-		type: 'TOGGLE_TODO',
-		id
+		type: 'UPDATE_TODO',
+		id,
+		updates
 	};
 };
 
@@ -51,5 +52,19 @@ export var addTodos = (todos)=>{
 	return {
 		type: 'ADD_TODOS',
 		todos
+	};
+};
+
+export var startToggleTodo = (id, completed)=>{
+	return (dispatch, getState)=>{
+		var todoRef = firebaseRef.child(`todos/${id}`);
+		var updates = {
+			completed,
+			completedAt: completed? moment().unix():null
+		};
+
+		return todoRef.update(updates).then(()=>{
+			dispatch(updateTodo(id, updates));
+		});
 	};
 };
